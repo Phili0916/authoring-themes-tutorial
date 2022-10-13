b42059196418f64dccdffabe687c6093a73dfff4
Philip Davis <p.davis@geofit.fr>
2022-10-13 10:37:01 +0200

    created data folder with events.yml file

---
+++ site/.cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\pdavis\\authoring-themes-tutorial\\site\\.cache\\dev-404-page.js"))
+}
+
