8ecc1091d2e50066a3f82f36029c300668c68376
Philip Davis <p.davis@geofit.fr>
2022-10-13 15:12:55 +0200

    added query data for eventList

--- site/.cache/page-ssr/routes/component---gatsby-theme-events-src-templates-events-jshead.js
+++ site/.cache/page-ssr/routes/component---gatsby-theme-events-src-templates-events-jshead.js
@@ -13,13 +13,29 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _site_public_page_data_sq_d_195173990_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../site/public/page-data/sq/d/195173990.json */ "./public/page-data/sq/d/195173990.json");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
 
 
-const EventsTemplate = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "TODO: Build the events page template");
+
+const EventsTemplate = () => {
+  const data = _site_public_page_data_sq_d_195173990_json__WEBPACK_IMPORTED_MODULE_0__.data;
+  const events = data.allEvent.nodes;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("p", null, "TODO: Build the events page template");
+};
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (EventsTemplate);
+
+/***/ }),
+
+/***/ "./public/page-data/sq/d/195173990.json":
+/*!**********************************************!*\
+  !*** ./public/page-data/sq/d/195173990.json ***!
+  \**********************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"data":{"allEvent":{"nodes":[{"id":"01f72681-93a8-58b8-b926-36acce303ba0","name":"DinosaurJS","startDate":"2019-06-20T00:00:00.000Z","endDate":"2019-06-21T00:00:00.000Z","location":"Denver, CO","url":"https://dinosaurjs.org/","slug":"/dinosaurjs"},{"id":"7099c688-4b82-59e8-a61e-c9c8327cfe94","name":"React Rally","startDate":"2019-08-22T00:00:00.000Z","endDate":"2019-08-23T00:00:00.000Z","location":"Salt Lake City, UT","url":"https://www.reactrally.com/","slug":"/react-rally"},{"id":"48c25eda-d441-5c2b-abc3-5ee8fb936091","name":"The Lead Developer","startDate":"2019-11-08T00:00:00.000Z","endDate":"2019-11-08T00:00:00.000Z","location":"Austin, TX","url":"https://austin2019.theleaddeveloper.com/","slug":"/the-lead-developer"},{"id":"7b7ca3b8-5413-584e-af92-a77bba99cbbe","name":"JSHeroes","startDate":"2020-04-23T00:00:00.000Z","endDate":"2020-04-24T00:00:00.000Z","location":"Cluj-Napoca, Romania","url":"https://jsheroes.io/","slug":"/jsheroes"}]}}}');
 
 /***/ })
 
